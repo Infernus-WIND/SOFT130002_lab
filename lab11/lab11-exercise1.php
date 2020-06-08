56fcdd589b207637dd73263f588cba9b2f57b62f
Infernus-WIND <1632459009@qq.com>
2020-06-08 19:05:59 +0800

    lab11

--- lab11/lab11-exercise1.php
+++ lab11/lab11-exercise1.php
@@ -1,11 +1,11 @@
 <html lang="en">
 <head>
 
-<!-- Latest compiled and minified Bootstrap Core CSS -->
-<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+    <!-- Latest compiled and minified Bootstrap Core CSS -->
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 
-   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-   <title>Exercise 11-1 | Using Cookies</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <title>Exercise 11-1 | Using Cookies</title>
 </head>
 
 <body>
@@ -15,8 +15,9 @@
 
 <?php
 session_start();
-function getLoginForm(){
-   return "
+function getLoginForm()
+{
+    return "
 <form action='' method='post' role='form'>
 <div class ='form-group'>
   <label for='username'>Username</label>
@@ -31,51 +32,54 @@ function getLoginForm(){
 </form>";
 }
 
-function validLogin(){
-    $pdo = new PDO(DBCONNSTRING,DBUSER,DBPASS);
+function validLogin()
+{
+    $pdo = new PDO(DBCONNSTRING, DBUSER, DBPASS);
     //very simple (and insecure) check of valid credentials.
     $sql = "SELECT * FROM Credentials WHERE Username=:user and Password=:pass";
 
     $statement = $pdo->prepare($sql);
-    $statement->bindValue(':user',$_POST['username']);
-    $statement->bindValue(':pass',$_POST['pword']);
+    $statement->bindValue(':user', $_POST['username']);
+    $statement->bindValue(':pass', $_POST['pword']);
     $statement->execute();
-    if($statement->rowCount()>0){
+    if ($statement->rowCount() > 0) {
         return true;
     }
     return false;
 }
-?>
- <div class="container theme-showcase" role="main">  
-      <div class="jumbotron">
-        <h1>
-<?php
-   require_once("config.php");
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if(validLogin()){
-        $_SESSION['Username']=$_POST['username'];
-        echo "Welcome ".$_SESSION['Username'];
-    }
-    else{
-        echo "login unsuccessful";
-    }
-}
-else{
-    echo "No Post detected";
-}
-?>
 
-</h1>
-      </div>
-<?php
-if (!isset($_SESSION['Username'])){
-    echo getLoginForm();
-}
-else{
-    echo "This is some content";
-}
 ?>
-     <a href="logout.php">logout</a>
- </div>
+<div class="container theme-showcase" role="main">
+    <div class="jumbotron">
+        <h1>
+            <?php
+            require_once("config.php");
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                if (validLogin()) {
+                    $_SESSION['Username'] = $_POST['username'];
+                    echo "Welcome " . $_SESSION['Username'];
+                } else {
+                    echo "login unsuccessful";
+                }
+            } else if (isset($_SESSION['Username'])) {
+                echo "Welcome " . $_SESSION['Username'];
+
+            } else {
+                echo "No Post detected";
+            }
+
+            ?>
+
+        </h1>
+    </div>
+    <?php
+    if (!isset($_SESSION['Username'])) {
+        echo getLoginForm();
+    } else {
+        echo "This is some content";
+    }
+    ?>
+    <a href="logout.php">logout</a>
+</div>
 </body>
 </html>
